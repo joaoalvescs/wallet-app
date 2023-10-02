7bd2ef2ccb358dc5a54d2514230b81e7defca709
João Alves <joaoalves.cs@gmail.com>
2023-10-01 22:54:56 -0300

    StatusBar configurada

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import AppLoading from 'expo-app-loading'
 import { View, Text } from 'react-native'
+import { StatusBar } from 'expo-status-bar'
 import { ThemeProvider } from 'styled-components/native'
 
 import {
@@ -35,6 +36,11 @@ const App = () => {
 
   return (
     <ThemeProvider theme={COLORS}>
+      <StatusBar 
+        style="dark" 
+        translucent
+        backgroundColor='transparent'
+      />
       <View
         style={{
           flex: 1,
