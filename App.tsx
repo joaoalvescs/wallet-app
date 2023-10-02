108296de43b2e63eeaaf8ace144640e6edfe671c
João Alves <joaoalves.cs@gmail.com>
2023-10-01 22:05:08 -0300

    Fontes configuradas.

--- App.tsx
+++ App.tsx
@@ -1,7 +1,35 @@
 import React from 'react'
+import AppLoading from 'expo-app-loading'
 import { View, Text } from 'react-native'
 
+import {
+  useFonts,
+  Poppins_300Light,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+  Poppins_800ExtraBold
+} from "@expo-google-fonts/poppins"
+
+import { DMSans_400Regular } from '@expo-google-fonts/dm-sans'
+import { DMSerifDisplay_400Regular } from '@expo-google-fonts/dm-serif-display'
+
 const App = () => {
+
+  const [fontsLoaded] = useFonts({
+    Poppins_300Light,
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+    Poppins_800ExtraBold,
+    DMSans_400Regular,
+    DMSerifDisplay_400Regular
+  })
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
+
   return (
     <View
       style={{
@@ -10,7 +38,7 @@ const App = () => {
         alignItems: "center"
       }}
     >
-      <Text> Wallet App </Text>
+      <Text> Wallet App</Text>
     </View>
   )
 }
